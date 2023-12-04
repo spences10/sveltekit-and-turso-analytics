2a16bdef12ce9963768aeba739b7ae060c650eb1
spences10 <spences10apps@gmail.com>
2023-12-04 08:41:31 +0000

    refactor: :recycle: move to turso folder

---
+++ src/lib/turso/schema.sql
@@ -0,0 +1,42 @@
+CREATE TABLE
+  user_session (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    ip_address TEXT NOT NULL,
+    session_start TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    session_end TIMESTAMP,
+    user_agent TEXT,
+    referrer TEXT,
+    session_duration INTEGER,
+    page_count INTEGER DEFAULT 0
+  );
+
+CREATE TABLE
+  page_analytics (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    slug TEXT NOT NULL,
+    pageviews INTEGER DEFAULT 0,
+    visits INTEGER DEFAULT 0,
+    uniques INTEGER DEFAULT 0,
+    avg_duration REAL,
+    bounce_rate REAL,
+    last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  );
+
+CREATE TABLE
+  page_visits (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    session_id INTEGER NOT NULL,
+    slug TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  );
+
+CREATE TABLE
+  session_geolocation (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    session_id INTEGER,
+    city TEXT,
+    region TEXT,
+    country TEXT,
+    location TEXT,
+    timezone TEXT
+  );
