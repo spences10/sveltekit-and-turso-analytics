dd438f49611596c0a46d5b20a4b0d6086b2d1829
spences10 <spences10apps@gmail.com>
2023-12-09 12:46:51 +0000

    refactor: :recycle: use client instance
    move and update schema

--- src/lib/turso/client.ts
+++ src/lib/turso/client.ts
@@ -2,23 +2,28 @@ import {
 	TURSO_DB_AUTH_TOKEN,
 	TURSO_DB_URL,
 } from '$env/static/private';
-import { createClient, type Client } from '@libsql/client/http';
+import { createClient, type Client } from '@libsql/client';
+
+let client_instance: Client | null = null;
 
 export const turso_client = (): Client => {
-	const url = TURSO_DB_URL?.trim();
-	if (url === undefined) {
-		throw new Error('TURSO_DB_URL is not defined');
-	}
-
-	const auth_token = TURSO_DB_AUTH_TOKEN?.trim();
-	if (auth_token === undefined) {
-		if (!url.includes('file:')) {
-			throw new Error('TURSO_DB_AUTH_TOKEN is not defined');
+	if (!client_instance) {
+		const url = TURSO_DB_URL?.trim();
+		if (url === undefined) {
+			throw new Error('TURSO_DB_URL is not defined');
 		}
-	}
 
-	return createClient({
-		url: TURSO_DB_URL as string,
-		authToken: TURSO_DB_AUTH_TOKEN as string,
-	});
+		const auth_token = TURSO_DB_AUTH_TOKEN?.trim();
+		if (auth_token === undefined) {
+			if (!url.includes('file:')) {
+				throw new Error('TURSO_DB_AUTH_TOKEN is not defined');
+			}
+		}
+
+		client_instance = createClient({
+			url: TURSO_DB_URL as string,
+			authToken: TURSO_DB_AUTH_TOKEN as string,
+		});
+	}
+	return client_instance;
 };
