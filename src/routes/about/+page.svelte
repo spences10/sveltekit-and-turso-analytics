c82daa20d47a57f8ba52bf5ce3216234cdc2a920
spences10 <spences10apps@gmail.com>
2023-12-09 20:47:38 +0000

    refactor: :recycle: update about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,15 +1,18 @@
-<div class="space-y-2 mb-7 prose prose-xl">
-	<h1
-		class="mb-4 text-3xl font-bold tracking-wide sm:text-4xl md:text-5xl lg:text-6xl text-accent"
-	>
-		About this
-	</h1>
-	<p class="mx-auto md:text-xl">
+<div class="prose prose-xl prose-headings:text-accent">
+	<h1>About this</h1>
+	
+	<p>
 		As the title suggests this is an experiment to see what analytics
 		could be recorded with Turso and SvelteKit.
 	</p>
-	<p class="mx-auto md:text-xl">Technologies used:</p>
-	<ul class="list-disc">
+	<p>
+		Session data stored for no longer than 24 hours and deleted after
+		the session ends.
+	</p>
+	<p>Location data is anonymised and stored with no related data.</p>
+
+	<h2>Technologies used:</h2>
+	<ul>
 		<li>SvelteKit</li>
 		<li>Turso</li>
 		<li>daisyUI</li>
