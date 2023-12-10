5bc7450deed7fb8d4296a2eb512bfc9d2e511552
spences10 <spences10apps@gmail.com>
2023-12-10 13:48:55 +0000

    refactor: :recycle: update queries

--- src/lib/turso/calculate-metrics.ts
+++ src/lib/turso/calculate-metrics.ts
@@ -5,49 +5,57 @@ export const calculate_metrics = async () => {
 
 	// SQL to calculate average session duration for each page
 	let avg_duration_sql = `
-    UPDATE page_analytics
-    SET avg_duration = (
-      SELECT AVG(session_duration)
-      FROM user_session
-      WHERE user_session.id IN (
-        SELECT session_id
-        FROM page_visits
-        WHERE page_visits.slug = page_analytics.slug
-      )
-    );
-  `;
+		UPDATE page_analytics
+		SET avg_duration = (
+			SELECT AVG(user_session.session_duration)
+			FROM user_session
+			JOIN page_visits ON user_session.id = page_visits.session_id
+			WHERE page_visits.slug = page_analytics.slug
+			AND date(user_session.session_start) = date(page_analytics.date)
+		)
+		WHERE EXISTS (
+			SELECT 1
+			FROM user_session
+			JOIN page_visits ON user_session.id = page_visits.session_id
+			WHERE page_visits.slug = page_analytics.slug
+			AND date(user_session.session_start) = date(page_analytics.date)
+		);
+	`;
 	await client.execute(avg_duration_sql);
 
 	// SQL to calculate bounce rate for each page
 	let bounce_rate_sql = `
-    UPDATE page_analytics
-    SET bounce_rate = (
-      SELECT CAST(COUNT(*) AS REAL) / (
-        SELECT COUNT(*)
-        FROM user_session
-        WHERE user_session.id IN (
-          SELECT session_id
-          FROM page_visits
-          WHERE page_visits.slug = page_analytics.slug
-        )
-      )
-      FROM user_session
-      WHERE page_count = 1
-      AND user_session.id IN (
-        SELECT session_id
-        FROM page_visits
-        WHERE page_visits.slug = page_analytics.slug
-      )
-    );
-  `;
+		UPDATE page_analytics
+		SET bounce_rate = (
+			SELECT CAST(COUNT(*) AS REAL) / (
+				SELECT COUNT(*)
+				FROM user_session
+				JOIN page_visits ON user_session.id = page_visits.session_id
+				WHERE page_visits.slug = page_analytics.slug
+				AND date(user_session.session_start) = date(page_analytics.date)
+			)
+			FROM user_session
+			JOIN page_visits ON user_session.id = page_visits.session_id
+			WHERE user_session.page_count = 1
+			AND page_visits.slug = page_analytics.slug
+			AND date(user_session.session_start) = date(page_analytics.date)
+		)
+		WHERE EXISTS (
+			SELECT 1
+			FROM user_session
+			JOIN page_visits ON user_session.id = page_visits.session_id
+			WHERE page_visits.slug = page_analytics.slug
+			AND date(user_session.session_start) = date(page_analytics.date)
+		);
+	`;
 	await client.execute(bounce_rate_sql);
 
 	// Cleanup old session data
 	let cleanup_sql = `
-    DELETE FROM user_session 
-    WHERE 
-      (session_end IS NOT NULL AND session_end < datetime('now', '-1 hour')) OR
-      (session_end IS NULL AND session_start < datetime('now', '-24 hours'));
-  `;
+		DELETE FROM user_session 
+		WHERE 
+			(session_end IS NOT NULL AND session_end < datetime('now', '-1 hour')) OR
+			(session_end IS NULL AND session_start < datetime('now', '-24 hours'));
+	`;
 	await client.execute(cleanup_sql);
 };
